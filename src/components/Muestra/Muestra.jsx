d74b37dc76f1524a2cbe8a9dca36b7bf47eb38ad
nahuek4 <nahuek.75@gmail.com>
2025-03-09 04:00:25 -0300

    prueba

--- src/components/Muestra/Muestra.jsx
+++ src/components/Muestra/Muestra.jsx
@@ -9,7 +9,7 @@ const Muestra = () => {
                 <img src="../../assets/icons/menu.webp" alt="menu" className="menunav" />
             </div>
             <div className="textos">
-                <h1 className="backtext">New website is</h1><span>coming soon</span>
+                <h1 className="backtext">New website iss</h1><span>coming soon</span>
             </div>
         </div>
     );
